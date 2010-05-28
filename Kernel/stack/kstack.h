7aff5478cc104cd4b825321450d37c10ad338bbe
Blake Loring <blake_l@mail.com>
2010-05-28 19:42:25 +0100

    Kernel stack moves to location now (Forgot to change it)

--- Kernel/stack/kstack.h
+++ Kernel/stack/kstack.h
@@ -1,6 +1,8 @@
 #ifndef _KERNEL_STACK_DEF_H_
 #define _KERNEL_STACK_DEF_H_
+#include <mm/virt_mm.h>
 
-#define KERNEL_STACK_START 0xC0000000
-#define KERNEL_STACK_SIZE 0x4000 //4kb stack
+#define KERNEL_STACK_START KERNEL_START + 0x20001000
+#define KERNEL_STACK_SIZE 0x1000 //4kb stack
+
 #endif

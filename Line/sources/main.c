db741db76ad4376bd72325850ee67512376ad649
Blake Loring <blake_l@mail.com>
2010-06-14 21:25:26 +0100

    Very minor revision to last checkout - folder shuffle

---
+++ Line/sources/main.c
@@ -0,0 +1,87 @@
+#include <types/memory.h>
+#include <common.h>
+#include <printf.h>
+#include <version/version.h>
+#include <syscall/syscall.h>
+#include <printf.h>
+
+char Data[1024];
+
+char Pointer[1024];
+
+unsigned int c_pointer = 0;
+
+DEFN_SYSCALL2(set_input_buffer, 1, const char*, unsigned int);
+DEFN_SYSCALL0(get_process_cpos, 2);
+DEFN_SYSCALL1(set_process_cpos, 3, unsigned int);
+
+void exec_cmd()
+{
+	printf("\n");
+
+	if (strcmp("exit", Pointer) == 0)
+	{
+		printf("Exit called\n");
+	}
+	else if (strcmp("help", Pointer) == 0)
+	{
+		printf("Line - Command line tool for SimpleOS\n");
+		printf("Compiled with SimpleOS version %i.%i.%i\n", VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION);
+		printf("No real functions yet.\n");
+	}
+	else
+	{
+		printf("Command %s not found\n", Pointer);
+	}
+}
+
+int start(int argc, void* argv)
+{
+	syscall_set_input_buffer(Data, 1024);
+
+	printf("Line - Executable compiled for SimpleOS version %i.%i.%i\n", VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION);
+
+	printf("Line.x:> ");
+	Pointer[0] = '\0';
+
+	for (;;)
+	{
+		MEM_LOC cp = syscall_get_process_cpos();
+
+		if (cp != 0)
+		{
+			unsigned int iter = 0;
+			for (iter = 0; iter < cp; iter++)
+			{
+				if (Data[iter] == '\r')
+				{
+					if (c_pointer != 0) {
+						exec_cmd();
+						printf("Line.x:> ");
+						c_pointer = 0;
+						Pointer[c_pointer] = '\0';
+					}
+				} else if (Data[iter] == '\b')
+				{
+					if (c_pointer > 0) {
+						printf("\b \b");
+						c_pointer = c_pointer - 1;
+						Pointer[c_pointer] = '\0';
+					}
+				}
+				else
+				{
+					printf("%c", Data[iter]);
+					Pointer[c_pointer] = Data[iter];
+					Pointer[c_pointer + 1] = '\0';
+					c_pointer = c_pointer + 1;
+				}
+			}
+
+			syscall_set_process_cpos(0);
+
+		}
+	}
+
+	return 32;
+}

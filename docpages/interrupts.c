abef033a34a88eab69f07448cb33529d469aefb9
Blake Loring <blake_l@mail.org>
2010-12-07 22:58:36 +0000

    Last big commit before submission - changes to the documentation mainly, removing all those ugly TODO's with documentation

--- docpages/interrupts.c
+++ docpages/interrupts.c
@@ -2,6 +2,5 @@
  * @page Interrupts
  * @section Interrupts
  * Interrupts are a method of event handling. There are both software interrupts within the CPU and hardware interrupts caused by electrical charges from the PIC.
- * [STUB Todo: Finish]
  *
  */
--- docpages/multitasking.c
+++ docpages/multitasking.c
@@ -3,5 +3,5 @@
  * 
  * The way Dawn achieves its initial type of multiprocessing is by swapping processes running within memory after a certain time frame, using a scheduler. This creates the illusion that more then one process is being processed at once even though Dawn is not currently able to utilize Dual-Core or multiple processors. (True as of Sun 13th of June 2010)
  *
- *[STUB: Todo: Finish]
+ *
  */

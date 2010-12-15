6a848c35b56ed7e4a9120f9a55f20deb23acdaec
Blake Loring <blake_l@mail.org>
2010-12-15 11:40:50 +0000

    Some corrections to the documentation

--- docpages/references_and_sources.c
+++ docpages/references_and_sources.c
@@ -1,8 +1,11 @@
 /**
  * @page ReferencesAndSources References and Sources
- * It is almost impossible to list every source, link and manual I peeked at to learn how to design and build a operating system. Therefore this page will contain the list of primary sources and links that I used and will leave a kind note to everybody else that contributes to the non-commercial operating system development scene.
+ * It is almost impossible to list every source, link and manual I peeked at to learn how to design and build a operating system. I tried however to list several of the more critical resources used during my project.
+ *
  * Intel x86 manuals
  * osdev.org
- * #osdev on freenode
+ * osdever.net
+ * The IRC channel #osdev on freenode
+ * Antonio, PHD Student at UCL who has been assisting me with programming theory and how to write good code
  * 
  */

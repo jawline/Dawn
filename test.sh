e677c6c009605c5eea84fe8ed8955d527a652e31
Blake Loring <blake_@parsed.uk>
2014-10-14 21:21:43 +0100

    test.sh now uses gksudo instead of requiring run with sudo

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 sh scripts/rebuild_components.sh
 sh scripts/rebuild_kernel.sh
 sh scripts/create_ramdisk.sh
-sh scripts/update_image.sh hdd.img
+gksudo sh scripts/update_image.sh hdd.img
 
 qemu-system-i386 -hda "hdd.img" -cpu coreduo -m 1000

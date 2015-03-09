228295c9d617db221a7e02681df519d104bdaad0
Blake Loring <blake_l@parsed.uk>
2015-03-09 14:16:15 +0000

    Update gpf.c

--- Kernel/arch/ia32/gpf/gpf.c
+++ Kernel/arch/ia32/gpf/gpf.c
@@ -2,12 +2,10 @@
 #include <panic/panic.h>
 #include <interrupts/interrupt_handler.h>
 
-void gpfHandler()
-{
+void gpfHandler() {
 	handleFatalProcessFault(FAULT_ID_GPF, "A GPF occured");
 }
 
-void initializeGeneralProtectionFaultHandler()
-{
+void initializeGeneralProtectionFaultHandler() {
 	registerInterruptHandler (13 , (isr_t) &gpfHandler);
 }

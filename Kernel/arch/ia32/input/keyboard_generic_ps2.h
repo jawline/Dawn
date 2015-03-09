c428cbf8e82aa79ba8ec3693ad9b7270affb79b4
Blake Loring <blake_l@parsed.uk>
2015-03-09 14:15:00 +0000

    Documentation

--- Kernel/arch/ia32/input/keyboard_generic_ps2.h
+++ Kernel/arch/ia32/input/keyboard_generic_ps2.h
@@ -1,11 +1,14 @@
 #ifndef _KEYBOARD_GENERIC_PS2_
 #define _KEYBOARD_GENERIC_PS2_
-
 #include <interrupts/interrupt_handler.h>
 #include <types/stdint.h>
 
 typedef void (*keyboard_callback) (unsigned char ascik);
 
+/**
+ * Initialise keyboard interrupt handlers and clear anything that's in the key buffer.
+ * Any keypresses after this is called (when IRQs are enabled) should be processed.
+ */
 void initializeKeyboard();
 
 #endif //_KEYBOARD_GENERIC_PS2_

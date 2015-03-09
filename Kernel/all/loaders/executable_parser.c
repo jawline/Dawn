027725b09891a30413375fdad06b62d5744edca9
Blake Loring <blake_l@parsed.uk>
2015-03-09 16:01:31 +0000

    Reformatting elf helper functions

--- Kernel/all/loaders/executable_parser.c
+++ Kernel/all/loaders/executable_parser.c
@@ -85,67 +85,60 @@ e32_sheader parseElfSectionHeader(fs_node_t* File, e32_header Header, unsigned i
 	return headerStruct;
 }
 
-//NOTE: Assumes fs_node_t* is available to readora
-void parseElfFile(e32info* info, fs_node_t* File)
-{
-	//Parse the main header
-	info->m_mainHeader = parseElfHeader(File);
+void parseElfFile(e32info* info, fs_node_t* file) {
+	unsigned int headerIterator;
+	
+	info->m_mainHeader = parseElfHeader(file);
 
 	//Allocate the space for the program headers
 	info->m_programHeaders = (e32_pheader*) malloc(sizeof(e32_pheader) * info->m_mainHeader.e_phnum);
 	info->m_numProgramHeaders = info->m_mainHeader.e_phnum;
 
-	//Iterate through and load each header
-	unsigned int headerIterator;
-	for (headerIterator = 0; headerIterator < info->m_numProgramHeaders; headerIterator++)
-	{
-		info->m_programHeaders[headerIterator] = parseElfProgramHeader(File, info->m_mainHeader, headerIterator);
+	//Load each header
+	for (headerIterator = 0; headerIterator < info->m_numProgramHeaders; headerIterator++) {
+		info->m_programHeaders[headerIterator] = parseElfProgramHeader(file, info->m_mainHeader, headerIterator);
 	}
 
 	//Allocate the space for the section headers
 	info->m_sectionHeaders = (e32_sheader*) malloc(sizeof(e32_sheader) * info->m_mainHeader.e_shnum);
 	info->m_numSectionHeaders = info->m_mainHeader.e_shnum;
 
-	//Iterate through and load each section header
-	for (headerIterator = 0; headerIterator < info->m_numSectionHeaders; headerIterator++)
-	{
-		info->m_sectionHeaders[headerIterator] = parseElfSectionHeader(File, info->m_mainHeader, headerIterator);
+	//Load each section header
+	for (headerIterator = 0; headerIterator < info->m_numSectionHeaders; headerIterator++) {
+		info->m_sectionHeaders[headerIterator] = parseElfSectionHeader(file, info->m_mainHeader, headerIterator);
 	}
-
 }
 
-//Free the ELF file info
-void freeElfFileInfo(e32info* info)
-{
-	if (info->m_programHeaders)
-	{
+void freeElfFileInfo(e32info* info) {
+	if (info->m_programHeaders) {
 		free(info->m_programHeaders);
 	}
-
-	if (info->m_sectionHeaders)
-	{
+	if (info->m_sectionHeaders) {
 		free(info->m_sectionHeaders);
 	}
 }
 
-//Check if a elf header is valid
-unsigned char elfHeaderValid(e32_header h)
-{
-	if ((h.elf_ident[ID_MAG0] == ELF_MAG0) && (h.elf_ident[ID_MAG1] == ELF_MAG1) && (h.elf_ident[ID_MAG2] == ELF_MAG2) && (h.elf_ident[ID_MAG3] == ELF_MAG3)) return 1;
+unsigned char elfHeaderValid(e32_header h) {
+	unsigned char mag0 = h.elf_ident[ID_MAG0] == ELF_MAG0;
+	unsigned char mag1 = h.elf_ident[ID_MAG1] == ELF_MAG1;
+	unsigned char mag2 = h.elf_ident[ID_MAG2] == ELF_MAG2;
+	unsigned char mag3 = h.elf_ident[ID_MAG3] == ELF_MAG3;
+	
+	if (mag0 && mag1 && mag2 && mag3) {
+		return 1;
+	}
+	
 	return 0;
 }
 
-unsigned int getElfHeaderClass(e32_header h)
-{
+unsigned int getElfHeaderClass(e32_header h) {
 	return h.elf_ident[ID_CLASS];
 }
 
-unsigned int getElfHeaderVersion(e32_header header)
-{
+unsigned int getElfHeaderVersion(e32_header header) {
 	return header.elf_ident[ID_VERSION];
 }
 
-unsigned int getElfHeaderData(e32_header header)
-{
+unsigned int getElfHeaderData(e32_header header) {
 	return header.elf_ident[ID_DATA];
 }

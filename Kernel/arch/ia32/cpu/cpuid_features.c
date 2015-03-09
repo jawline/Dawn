cb056c6244f1db479b71fdef13d2eaadf13667bf
Blake Loring <blake_l@parsed.uk>
2015-03-09 13:48:25 +0000

    Refactor cpuid_features

--- Kernel/arch/ia32/cpu/cpuid_features.c
+++ Kernel/arch/ia32/cpu/cpuid_features.c
@@ -1,11 +1,6 @@
 #include "cpu.h"
 
-//Function: getCpuidFeatures
-//Arguments: None
-//Returns: eax register of the CPUID call (Features)
-//Description: Calls cpuid and returns the features register
-unsigned long getCpuidFeatures()
-{
+unsigned long queryCpuidFeatures() {
 	unsigned long input = 0x1;
 	unsigned long cpuid_features_result = 0;
 
@@ -16,12 +11,11 @@ unsigned long getCpuidFeatures()
 	return cpuid_features_result;
 }
 
-//Function cpuidFeatures()
-//Arguments: None
-//Returns: features integer returned from CPUID instruction
-//Description: Checks if cpuid is supported, if not return null if true return getCpuidFeatures()
-unsigned long cpuidFeatures()
-{
-	if (!cpuidSupported()) return 0;
-	return getCpuidFeatures();
+unsigned long cpuidFeatures() {
+
+	if (!cpuidSupported()) { 
+		return 0;
+	}
+
+	return queryCpuidFeatures();
 }

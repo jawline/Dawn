852098a2b5671db1d6185cd035e5a5eb9154e5ce
Blake Loring <blake_l@parsed.uk>
2015-03-09 13:50:28 +0000

    Update usermode.c

--- Kernel/arch/ia32/usermode/usermode.c
+++ Kernel/arch/ia32/usermode/usermode.c
@@ -1,28 +1,24 @@
 #include <stack/kstack.h>
 
 void switchToUserMode() {
-
 	setKernelStack(KERNEL_STACK_START);
-
 	__asm__ volatile("  \
-      cli; \
-      mov $0x23, %ax; \
-      mov %ax, %ds; \
-      mov %ax, %es; \
-      mov %ax, %fs; \
-      mov %ax, %gs; \
-                    \
-       \
-      mov %esp, %eax; \
-      pushl $0x23; \
-      pushl %eax; \
-      pushf; \
-      popl %eax; \
-      orl $0x200, %eax; \
-      pushl %eax; \
-      pushl $0x1B; \
-      push $1f; \
-      iret; \
-    1: \
-      ");
+	      	cli; \
+		mov $0x23, %ax; \
+	      	mov %ax, %ds; \
+	      	mov %ax, %es; \
+	      	mov %ax, %fs; \
+	      	mov %ax, %gs; \
+	                      \
+	      	mov %esp, %eax; \
+	      	pushl $0x23; \
+	      	pushl %eax; \
+	      	pushf; \
+	      	popl %eax; \
+	      	orl $0x200, %eax; \
+	      	pushl %eax; \
+	      	pushl $0x1B; \
+	      	push $1f; \
+	      	iret; \
+	    	1: ");
 }

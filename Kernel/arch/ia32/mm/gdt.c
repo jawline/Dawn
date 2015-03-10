69f96e59fb2eb303e53499591923667eb40164ac
Blake Loring <blake_l@parsed.uk>
2015-03-10 09:18:04 +0000

    Formatting of gdt.c

--- Kernel/arch/ia32/mm/gdt.c
+++ Kernel/arch/ia32/mm/gdt.c
@@ -4,33 +4,29 @@
 gdt_entry_t gdt_entries[NUM_GDT_ENTRIES];
 gdt_ptr_t   gdt_ptr;
 
-void gdt_set_gate(unsigned int, uint32_t, uint32_t, uint8_t, uint8_t);
+void gdtSetGate(unsigned int, uint32_t, uint32_t, uint8_t, uint8_t);
 extern void gdt_flush(uint32_t);
 
-//Global descriptor table setup
-void initializeGdt()
-{
+void initializeGdt() {
    memset(&gdt_entries, 0, sizeof(gdt_entry_t) * NUM_GDT_ENTRIES);
 
    gdt_ptr.limit = (sizeof(gdt_entry_t) * NUM_GDT_ENTRIES) - 1;
    gdt_ptr.base  = (uint32_t)&gdt_entries;
 
-   gdt_set_gate(0, 0, 0, 0, 0);                // Null segment
-   gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment (Present, ring0, Executable)
-   gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data segment (Present, ring0)
-   gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User mode code segment (Present, ring3, Executable)
-   gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User mode data segment (Present, ring3)
+   gdtSetGate(0, 0, 0, 0, 0);                // Null segment
+   gdtSetGate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment (Present, ring0, Executable)
+   gdtSetGate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data segment (Present, ring0)
+   gdtSetGate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User mode code segment (Present, ring3, Executable)
+   gdtSetGate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User mode data segment (Present, ring3)
 
    flushGdt();
 }
 
-void flushGdt()
-{
+void flushGdt() {
    gdt_flush((uint32_t)&gdt_ptr);
 }
 
-void gdt_set_gate(unsigned int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran)
-{
+void gdtSetGate(unsigned int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran) {
    gdt_entries[num].base_low    = (base & 0xFFFF);
    gdt_entries[num].base_middle = (base >> 16) & 0xFF;
    gdt_entries[num].base_high   = (base >> 24) & 0xFF;

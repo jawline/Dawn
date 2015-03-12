6468af67a1c84894f71e8dd03796e79fe5d8b8e7
Blake Loring <blake_l@parsed.uk>
2015-03-12 10:32:41 +0000

    Improve postbox

--- Kernel/all/process/postbox.c
+++ Kernel/all/process/postbox.c
@@ -45,8 +45,8 @@ process_message postboxPeek(process_postbox* pb, process_message* dest) {
 
 void postboxPush(process_postbox* pb, process_message* msg) {
 
+	//If there is no head then create a new list
 	if (pb->first == 0) {
-		//Create pb->first
 		postbox_message_entry* new_entry = malloc(
 				sizeof(postbox_message_entry));
 		new_entry->data = *msg;
@@ -58,19 +58,13 @@ void postboxPush(process_postbox* pb, process_message* msg) {
 		postbox_message_entry* last = pb->first;
 
 		//Find the last entry
-		while (1) {
-			if (last->next == 0) {
-				break;
-			} else {
-				last = last->next;
-			}
+		while (last->next) {
+			last = last->next;
 		}
 
-		postbox_message_entry* new_entry = malloc(
-				sizeof(postbox_message_entry));
+		postbox_message_entry* new_entry = malloc(sizeof(postbox_message_entry));
 		new_entry->data = *msg;
 		new_entry->next = 0;
-
 		last->next = new_entry;
 	}
 }

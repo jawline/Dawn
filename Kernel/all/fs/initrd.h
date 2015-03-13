ef4acd1e2d587be14b147333ede02aa47482d659
Blake Loring <blake_l@parsed.uk>
2015-03-13 13:24:21 +0000

    Change ramdisk init signature

--- Kernel/all/fs/initrd.h
+++ Kernel/all/fs/initrd.h
@@ -2,6 +2,9 @@
 #define _INITIAL_RAMDISK_FILESYSTEM_H_
 #include <fs/vfs.h>
 
-fs_node_t* initialize_initrd(uint32_t start, char * name, fs_node_t * parent);
+/**
+ * Initialise the initial ramdisk, adding it to the filesystem
+ */
+fs_node_t* initialiseRamdisk(void* ramdiskLocation, char const* name, fs_node_t* parent);
 
 #endif //_INITIAL_RAMDISK_FILESYSTEM_H_

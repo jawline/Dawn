b9b6b8a27020175ff66a5ad7539c773482e697d9
Blake Loring <blake_l@parsed.uk>
2015-03-13 09:40:01 +0000

    Formatting

--- Kernel/all/debug/debugprint.c
+++ Kernel/all/debug/debugprint.c
@@ -1,22 +1,13 @@
 #include <debug/debugprint.h>
 
-void printDebugMessage(char* fmt, ...)
-{
-	if (getDebugMode() == 1)
-	{
-		static char buf [2096]; //Maximum size to be printed is 1024 chars.
+void printDebugMessage(char* fmt, ...) {
+	if (getDebugMode()) {
+		static char buf [1024*4]; //Maximum size to be printed is 4kb.
 		va_list args;
-
-		int i;
-	 
 		va_start(args, fmt);
-
-		i = vsprintf(buf,fmt,args);
-
+		int i = vsprintf(buf,fmt,args);
 		va_end(args);
-
 		buf[i] = '\0';
-
 		prints(buf);
 	}
 }

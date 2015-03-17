8e199ab617244c10cfe98e571a350a5492ecda4f
Blake Loring <blake_l@parsed.uk>
2015-03-17 15:48:27 +0000

    Update sleep.h

--- API/headers/process/sleep.h
+++ API/headers/process/sleep.h
@@ -4,23 +4,16 @@
 #include <syscall/syscall.h>
 
 /**
- * @ingroup Process Managment
- * 
- * @brief This function allows a program to pass up any remaining processing time it may have been allocated by the scheduler and sleeps it untill the scheduler assigns it more processing time
- * @param None
- * @return None
+ * This function allows a program to pass up any remaining processing time 
+ * it has been allocated by the scheduler and sleeps it until the scheduler
+ * allocates it more time.
  */
-
 void sleepProcess();
 
 /**
- * @ingroup Process Managment
- * 
- * @brief This function sleeps untill a number have ticks have passed (Note: Not precise due to the fact that this sleep function may leave the process idled tell past the time given in ticks)
- * @param unsigned long ticks for the amount of time the process wants to sleep
- * @return None
+ * This function sleeps for at least the number of ticks specified.
+ * (Note: It may sleep for longer)
  */
-
 void sleepUnprecise(unsigned long ticks);
 
 #endif //_PROCESS_SLEEP_API_

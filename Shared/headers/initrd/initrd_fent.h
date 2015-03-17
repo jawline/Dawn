29a2832ee26759b8556bcafc4646208981b095a9
Blake Loring <blake_l@parsed.uk>
2015-03-17 12:17:50 +0000

    Docs on initrd_fent

--- Shared/headers/initrd/initrd_fent.h
+++ Shared/headers/initrd/initrd_fent.h
@@ -1,10 +1,15 @@
 #ifndef _INITIAL_RAMDISK_FILE_ENTRY_
 #define _INITIAL_RAMDISK_FILE_ENTRY_
 
+/**
+ * Ramdisk file descriptor
+ * Starts with the name of the name of the file, then its
+ * start location in the ramdisk and its size in bytes
+ */
 struct initrd_fent {
 	char name[128];
-	uint32_t start; //Offset of the data from the start of the RAM disk
-	uint32_t size; //Size (in bytes) of the file
+	uint32_t start;
+	uint32_t size;
 } __attribute__((packed));
 
 #endif

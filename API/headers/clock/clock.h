86cdb02d032c66a0a35db672ea430bd6b2b1b7dd
Blake Loring <blake_l@parsed.uk>
2015-03-17 15:38:38 +0000

    Docs on clock.h

--- API/headers/clock/clock.h
+++ API/headers/clock/clock.h
@@ -3,23 +3,13 @@
 #include <syscall/syscall.h>
 
 /**
- * @ingroup Timing
- * 
- * @brief This function returns the number of ticks taht have occured since the system booted
- * @param None
- * @return unsigned long with the value of the number of system ticks that have occured since the PIT was initialized
+ * This function returns the number of ticks that have occured since the system booted
  */
-
 extern unsigned long clock();
 
 /**
- * @ingroup Timing
- * 
- * @brief This function returns the number of ticks that occur every second
- * @param None
- * @return unsigned long with the value of the number of ticks that occur per second.
+ * This function returns the number of ticks that occur every second
  */
-
 extern unsigned long getClocksPerSecond();
 
 #endif //_KERNEL_CLOCK_API_

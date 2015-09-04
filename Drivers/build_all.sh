abedef98b9729a3df418c811b07f27b2cabfa0fe
Blake Loring <blake_l@parsed.uk>
2015-09-04 09:44:25 +0100

    Fixed setup scripts

--- Drivers/build_all.sh
+++ Drivers/build_all.sh
@@ -5,15 +5,15 @@ for direc in $Dirlist ; do
 	    if [ "$direc" = "." ]; then
                echo Not evaluating .
 	    else
-		echo Compiing $direc
-		cd $direc
+			echo Compiing $direc
+			cd $direc
 
-		make clean
-		make
+			make clean
+			make
 
-		cd ..
-		cp $direc/Build/* ../bin/RamdiskFolder
-
-            fi
+			cd ..
+			mkdir ../bin/RamdiskFolder/
+			cp $direc/Build/* ../bin/RamdiskFolder/
+        fi
             
 done

3807b063dfa69cac2d5e8d147ea21194cef3a8c4
Blake Loring <blake_l@parsed.uk>
2016-03-29 10:33:12 +0100

    Update io.c

--- Kernel/all/lib/io.c
+++ Kernel/all/lib/io.c
@@ -1,36 +1,30 @@
 #include <lib/io.h>
 
-void outb(uint16_t port, uint8_t value)
-{
+void outb(uint16_t port, uint8_t value) {
 	__asm__ volatile ("outb %1, %0" : : "dN" (port), "a" (value));
 }
 
-uint8_t inb(uint16_t port)
-{
+uint8_t inb(uint16_t port) {
    uint8_t ret;
    __asm__ volatile ("inb %1, %0" : "=a" (ret) : "dN" (port));
    return ret;
 }
 
-void outw(uint16_t port, uint16_t value)
-{
+void outw(uint16_t port, uint16_t value) {
 	__asm__ volatile ("outw %1, %0" : : "dN" (port), "a" (value));
 }
 
-uint16_t inw(uint16_t port)
-{
+uint16_t inw(uint16_t port) {
    uint16_t ret;
    __asm__ volatile ("inw %1, %0" : "=a" (ret) : "dN" (port));
    return ret;
 }
 
-void outl(uint16_t port, uint32_t value)
-{
+void outl(uint16_t port, uint32_t value) {
 	__asm__ volatile ("outl %1, %0" : : "dN" (port), "a" (value));
 }
 
-uint32_t inl(uint16_t port, uint32_t value)
-{
+uint32_t inl(uint16_t port, uint32_t value) {
 	uint32_t ret;
 	__asm__ volatile("inl %1, %0" : "=a" (ret) : "dN" (port));
 	return ret;

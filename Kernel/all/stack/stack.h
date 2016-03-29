c4bb3c5f49d3aaa0ef66167a7d2b562adab6786b
Blake Loring <blake_l@parsed.uk>
2016-03-29 09:32:52 +0100

    Update stack.h

--- Kernel/all/stack/stack.h
+++ Kernel/all/stack/stack.h
@@ -7,6 +7,10 @@
 
 typedef void* stack_t;
 
+/**
+ * Move the stack from the initial stack pointer to a new position in memory, (Stack expands downard so the start location is the highest address used
+ * and -size is the earliest address used)
+ */
 stack_t moveStack(stack_t new_start, size_t size, size_t initial_esp);
 
 #endif //_STACK_DEF_H_

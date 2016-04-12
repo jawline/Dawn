ea28ab25694cb8fc0f8507affac74695a5227599
Blake Loring <blake_l@parsed.uk>
2016-04-12 10:15:02 +0100

    Update get_info.c

--- API/sources/process/get_info.c
+++ API/sources/process/get_info.c
@@ -9,25 +9,20 @@ DEFN_SYSCALL1(valid_process, 16, unsigned int);
 DEFN_SYSCALL1(get_processing_time, 18, unsigned int);
 DEFN_SYSCALL2(get_process_name, 19, char*, unsigned int);
 
-int getProcessID(unsigned int n)
-{
+int getProcessID(unsigned int n) {
 	int pid = syscall_get_process_id(n);
 	return pid;
 }
 
-process_info_t getProcessInfo(int pid)
-{
+process_info_t getProcessInfo(int pid) {
 	process_info_t info;
 	memset((void*)&info, 0, sizeof(process_info_t));
 
-	if (syscall_valid_process(pid) == 1)
-	{
+	if (syscall_valid_process(pid) == 1) {
 		info.pID = pid;
 		info.processingTime = syscall_get_processing_time(pid);
 		syscall_get_process_name(info.Name, pid);
-	}
-	else
-	{
+	} else {
 		info.pID = -1;
 	}
 

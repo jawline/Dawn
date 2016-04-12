2fd6191b83e7d294615480b60d2a3da2994608d2
Blake Loring <blake_l@parsed.uk>
2016-04-12 10:17:34 +0100

    Update memory.c

--- API/sources/system/memory.c
+++ API/sources/system/memory.c
@@ -3,12 +3,10 @@
 DEFN_SYSCALL0(num_free_frames, 7);
 DEFN_SYSCALL0(get_page_size, 8);
 
-MEM_LOC getNumberOfFreeFrames()
-{
+MEM_LOC getNumberOfFreeFrames() {
 	return syscall_num_free_frames();
 }
 
-unsigned long getPageSize()
-{
+unsigned long getPageSize() {
 	return syscall_get_page_size();
 }

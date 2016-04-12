4f9256fab9751ad9f9b88dd44b3478905eb19f5e
Blake Loring <blake_l@parsed.uk>
2016-04-12 10:17:14 +0100

    Update sleep.c

--- API/sources/process/sleep.c
+++ API/sources/process/sleep.c
@@ -2,12 +2,10 @@
 
 DEFN_SYSCALL0(block_process, 5);
 
-//Force the process to sleep
 void sleepProcess() {
 	syscall_block_process();
 }
 
-//Unprecise sleep routine that just waits untill a certain amount of time has passed and gives up processing time untill then
 void sleepUnprecise(unsigned long ticks) {
 	unsigned long tell = clock() + ticks;
 	while (clock() < tell) {

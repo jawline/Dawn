debba4e4dfda5ab1c14db749b78b61f762eb6210
Blake Loring <blake_l@parsed.uk>
2016-04-12 10:15:35 +0100

    Update postbox_api.c

--- API/sources/process/postbox_api.c
+++ API/sources/process/postbox_api.c
@@ -13,20 +13,17 @@ DEFN_SYSCALL0(postbox_has_next, 3);
 //Sets the flags on this process's postbox
 DEFN_SYSCALL1(postbox_set_flags, 6, uint32_t);
 
-unsigned char postboxHasNext()
-{
+unsigned char postboxHasNext() {
 	return syscall_postbox_has_next();
 }
 
-process_message postboxGetNext()
-{
-	process_message Msg;
-	syscall_postbox_read_top(&Msg);
+process_message postboxGetNext() {
+	process_message msg;
+	syscall_postbox_read_top(&msg);
 	syscall_postbox_pop_top();
-	return Msg;
+	return msg;
 }
 
-void postboxSetFlags(uint32_t flags)
-{
+void postboxSetFlags(uint32_t flags) {
 	syscall_postbox_set_flags(flags);
 }

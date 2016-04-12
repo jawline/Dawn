dedce9d5288250bdca1d8c7a56330df174028976
Blake Loring <blake_l@parsed.uk>
2016-04-12 10:13:45 +0100

    Update clock.c

--- API/sources/clock/clock.c
+++ API/sources/clock/clock.c
@@ -3,12 +3,10 @@
 DEFN_SYSCALL0(clocks_per_second, 10);
 DEFN_SYSCALL0(get_clock, 11);
 
-unsigned long getClocksPerSecond()
-{
+unsigned long getClocksPerSecond() {
 	return syscall_clocks_per_second();
 }
 
-unsigned long clock()
-{
+unsigned long clock() {
 	return syscall_get_clock();
 }

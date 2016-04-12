a468fed0799539475cf977590e3d1d002b60d4d2
Blake Loring <blake_l@parsed.uk>
2016-04-12 10:14:25 +0100

    Update end_process.c

--- API/sources/process/end_process.c
+++ API/sources/process/end_process.c
@@ -3,14 +3,12 @@
 DEFN_SYSCALL0(kill_current_process, 15);
 DEFN_SYSCALL1(request_exit, 20, int);
 
-void killcurrentprocess()
-{
+void killcurrentprocess() {
 	syscall_kill_current_process();
 	for (;;) {}
 }
 
-void exit(int value)
-{
+void exit(int value) {
 	syscall_request_exit(value);
 	for (;;) {}
 }

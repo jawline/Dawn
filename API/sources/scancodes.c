875c509c3b739d91766c4d809f4d1968104c5f96
Blake Loring <blake_l@parsed.uk>
2016-04-12 10:13:25 +0100

    Update scancodes.c

--- API/sources/scancodes.c
+++ API/sources/scancodes.c
@@ -2,8 +2,7 @@
 
 DEFN_SYSCALL2(scancode_to_asci, 4, unsigned char, uint32_t);
 
-char getAsciFromScancode(unsigned char sc, uint32_t flags)
-{
+char getAsciFromScancode(unsigned char sc, uint32_t flags) {
 	char ch = syscall_scancode_to_asci(sc, flags);
 	return ch;
 }
